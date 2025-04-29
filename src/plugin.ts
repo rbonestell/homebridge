6c8f0ff07e78629abc81b0a9251654a55c5aa417
Bobby Bonestell <rbonestell@gmail.com>
2025-04-28 19:08:30 -0600

    Removed SecretStore from plugin initailizer method

--- src/plugin.ts
+++ src/plugin.ts
@@ -22,7 +22,6 @@ import { satisfies } from 'semver'
 
 import { Logger } from './logger.js'
 import { PluginManager } from './pluginManager.js'
-import { SecretStore } from './secretStore.js'
 import getVersion from './version.js'
 
 const log = Logger.internal
@@ -34,7 +33,6 @@ export class Plugin {
   private readonly pluginName: PluginName
   private readonly scope?: string // npm package scope
   private readonly pluginPath: string // like "/usr/local/lib/node_modules/homebridge-lockitron"
-  private secretStore?: SecretStore // used to store secrets for the plugin
   public disabled = false // mark the plugin as disabled
 
   // ------------------ package.json content ------------------
@@ -233,9 +231,6 @@ major incompatibility issues and thus is considered bad practice. Please inform 
       throw new Error('Tried to initialize a plugin which hasn\'t been loaded yet!')
     }
 
-    // Initialize SecretStore for plugin instance
-    this.secretStore = new SecretStore(api.user?.persistPath(), '', this.pluginName)
-
-    return this.pluginInitializer(api, this.secretStore)
+    return this.pluginInitializer(api)
   }
 }
